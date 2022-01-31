394611ef8d34fe844b1316047b4256b0b0b6622a
andrewphm <andrpham@gmail.com>
2022-01-31 15:35:59 -0500

    Add responsive styling to font

--- src/components/common/InspireBanner.jsx
+++ src/components/common/InspireBanner.jsx
@@ -13,9 +13,9 @@ const InspireBanner = ({ img, title, description, buttonText }) => {
         />
 
         <div className="absolute top-10 left-10 w-1/2 space-y-3 lg:left-20 lg:top-20">
-          <h3 className="text-3xl font-medium ">{title}</h3>
-          <p className="font-semibold">{description}</p>
-          <button className="text-sm text-white bg-gray-900 px-4 py-2 rounded-lg hover:scale-105 shadow-md hover:shadow-lg">
+          <h3 className="text-3xl font-medium lg:text-4xl ">{title}</h3>
+          <p className="font-semibold lg:text-xl">{description}</p>
+          <button className="text-sm text-white lg:text-xl bg-gray-900 px-4 py-2 rounded-lg hover:scale-105 shadow-md hover:shadow-lg">
             {buttonText}
           </button>
         </div>
