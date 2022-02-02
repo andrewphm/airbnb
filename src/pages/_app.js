cfa0edd127e4cdad2cf6b705142538c0f19b19e6
andrewphm <andrpham@gmail.com>
2022-02-01 20:20:00 -0500

    Add badrap progress bar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,20 @@ import '../styles/globals.css';
 import store from '../redux/store';
 import { Provider } from 'react-redux';
 
+import ProgressBar from '@badrap/bar-of-progress';
+
+import Router from 'next/router';
+
+const progress = new ProgressBar({
+  size: 3,
+  color: '#FE595E',
+  className: 'z-50',
+});
+
+Router.events.on('routeChangeStart', progress.start);
+Router.events.on('routeChangeComplete', progress.finish);
+Router.events.on('routeChangeError', progress.finish);
+
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
